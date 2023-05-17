db433541f605bc5991c2b77f2d7207f477e673af
fracs31 <pist.fracs95@gmail.com>
2023-05-17 09:30:55 +0200

    FIX: indent

--- public_html/index.php
+++ public_html/index.php
@@ -17,21 +17,21 @@
             </header>
 
             <form method="post" id="payment-form" action="<?php echo $baseUrl;?>checkout.php">
-                <section>
-                    <label for="amount">
-                        <span class="input-label">Amount</span>
-                        <div class="input-wrapper amount-wrapper">
-                            <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                        </div>
-                    </label>
-
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin"></div>
+              <section>
+                <label for="amount">
+                    <span class="input-label">Amount</span>
+                    <div class="input-wrapper amount-wrapper">
+                      <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
                     </div>
-                </section>
+                </label>
 
-                <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button class="button" type="submit"><span>Test Transaction</span></button>
+                <div class="bt-drop-in-wrapper">
+                  <div id="bt-dropin"></div>
+                </div>
+              </section>
+
+              <input id="nonce" name="payment_method_nonce" type="hidden" />
+              <button class="button" type="submit"><span>Test Transaction</span></button>
             </form>
         </div>
     </div>
